fbc8d09aad85c5390488566ef2b0ce635cd2ccbb
Angel Guerrero <lasantiagoguerrero@gmail.com>
2021-10-09 03:28:16 -0500

    feat: instalación de tailwindcss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { defineProps, ref } from 'vue'
 
 defineProps({
   msg: String
@@ -9,32 +9,24 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="text-center">
+    <h1 class="text-2xl font-bold text-gray-800">{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <button
+      type="button"
+      @click="count++"
+      class="
+        px-2
+        py-1
+        my-2
+        text-white
+        bg-green-500
+        rounded
+        shadow
+        hover:bg-gray-700
+      "
+    >
+      count is: {{ count }}
+    </button>
+  </div>
 </template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
