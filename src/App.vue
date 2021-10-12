6f93d3ac6f52405ed8a02d5f9d47ff7838bac5ac
Angel Guerrero <lasantiagoguerrero@gmail.com>
2021-10-12 10:39:35 -0500

    feat: agregando componente para ingresar datos

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatContainer from './components/ChatContainer.vue'
 </script>
 
 <template>
-  <div class="grid min-h-screen place-items-center">
-    <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="grid min-h-screen place-items-end bg-gray-300">
+    <ChatContainer />
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
+const generateColorClass = (variable) => {
+  return ({ opacityValue }) =>
+    opacityValue
+      ? `rgba(var(--${variable}), ${opacityValue})`
+      : `rgb(var(--${variable}))`
+}
+
+const textColor = {
+  primary: generateColorClass('text-primary'),
+  secondary: generateColorClass('text-secondary'),
+  tertiary: generateColorClass('text-tertiary'),
+  accent: generateColorClass('text-accent'),
+  contrast: generateColorClass('text-contrast'),
+}
+
+const backgroundColor = {
+  primary: generateColorClass('bg-primary'),
+  secondary: generateColorClass('bg-secondary'),
+  tertiary: generateColorClass('bg-tertiary'),
+}
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      textColor,
+      backgroundColor,
+    },
   },
   variants: {
     extend: {},
